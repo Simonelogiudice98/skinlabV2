9ff987670e360149344724a1ab2c553dfdf2475b
simonelogiudice98 <logiusimone@gmail.com>
2026-08-24 18:52:11 +0200

    fix whatsapp

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,2 @@
    export const WHATSAPP_LINK:string =
-  "https://wa.me/447000000000?text=Hi%20SkinLab%20Medical%2C%20I%27d%20like%20to%20book%20a%20consultation.";
+  "https://wa.me/447841028347?text=Hi%20SkinLab%20Medical%2C%20I%27d%20like%20to%20book%20a%20consultation.";
